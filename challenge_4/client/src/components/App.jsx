3e5e1dbe7c3f31fa3656b26624e019b7b1a1fb4e
WarrenMfg <warrenmfg@icloud.com>
2020-01-16 14:50:54 -0500

    front and back scaffolding progress

---
+++ challenge_4/client/src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Mini apps - Challenge 4</h1>
+        <h2>Connect Four</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,8 @@
+const express = require('express');
+const app = express();
 
+app.use(express.static('./client/dist'));
+
+module.exports.app = app;
+app.listen(process.env.PORT || 8080, () => console.log('Listening on port 8080!'));
 
